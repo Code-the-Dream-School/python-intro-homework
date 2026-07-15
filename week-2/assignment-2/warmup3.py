cd37436b2c98072bf0258353717a289ff3bbb60f
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-14 20:41:49 -0400

    add warmup3.py

---
+++ week-2/assignment-2/warmup3.py
@@ -0,0 +1,6 @@
+#194c8fa (HEAD -> assignment-2, origin/assignment-2) add warmup2.py
+#294742d (origin/main, origin/HEAD, main) add week 2 folder
+#58d836d Add files via upload
+#d8c0c05 Update README.md
+  
+
