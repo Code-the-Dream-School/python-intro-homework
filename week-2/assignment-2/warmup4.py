4ab4081c9467bdf465d7adf487b87c44c4b129c0
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-14 20:43:27 -0400

    add warmup4.py

---
+++ week-2/assignment-2/warmup4.py
@@ -0,0 +1,14 @@
+name = input('what is your name? ')
+print(f'Your name is {name}')
+
+#File "c:\Users\genes\python_class\working\python-intro-homework\week-2\assignment-2\warmup4.py", line 2, in <module>
+  #  print(f'Your name is {naame}')
+           #               ^^^^^
+#NameError: name 'naame' is not defined. Did you mean: 'name'?
+
+#My error was first I wrote the wrong varible name on line 5 
+# My second error was I didnt write input in line 1 so the user is not 
+# able to even type their own name
+
+#I fixed my error by correct my variable name and including the word input on line 1
+
