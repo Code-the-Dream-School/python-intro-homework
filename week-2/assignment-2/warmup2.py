6579456537927cf67b89a717b9613627a2067e2b
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-21 17:50:26 -0400

    add warmup2.py

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -1,8 +1,10 @@
 #navigations commands I used:
 # cd python-intro-homework
+#ls
 #cd week-2
 #cd assignment-2
-#mkdir warmup2.py
+#pwd
+
 
 date = input("What is today's date? ")
 print(f'You said today is {date}.')
