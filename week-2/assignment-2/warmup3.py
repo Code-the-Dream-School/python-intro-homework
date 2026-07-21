fd355160889ad7296adacb720f459458cf5e5a4b
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-21 17:51:39 -0400

    add warmup3.py

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,6 +1,7 @@
-#194c8fa (HEAD -> assignment-2, origin/assignment-2) add warmup2.py
-#294742d (origin/main, origin/HEAD, main) add week 2 folder
-#58d836d Add files via upload
-#d8c0c05 Update README.md
+# git log --oneline output:
+# 194c8fa add warmup2.py
+# 294742d add week 2 folder
+# 58d836d Add files via upload
+# d8c0c05 Update README.md
   
 print("This week I learned how to use Python input, work with strings, and run Git commands in the terminal!")
