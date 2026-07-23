06d79987c6db8e56e135e7fdb6cd531ef3468651
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-22 20:42:37 -0400

    add warmup4.py

---
+++ week-3/assignment-3/warmup4.py
@@ -0,0 +1,15 @@
+number = int(input('Enter a number: '))
+
+if number > 0: 
+    print(f'{number} is positive.')
+elif number < 0: 
+    print(f'{number} is negative.')
+else:
+    print(f'{number} is zero. ')
+
+if number % 2 == 0:
+    print(f'{number} is even. ')
+
+else: 
+    print(f'{number} is odd.')
+    
