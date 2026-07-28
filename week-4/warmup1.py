a2c2ca61e08944bac9ea2a2e12531b433c248052
kfbmartin <your@email.com>
2026-07-28 10:11:36 -0400

    fixing warmup1 to only asks for the four middle items

--- week-4/warmup1.py
+++ week-4/warmup1.py
@@ -2,5 +2,5 @@
 numbers = [27, 40, 45, 50, 51, 52, 70, 99]
 print(f"First: {numbers[0]}")
 print(f"Last: {numbers[-1]}")
-print(f"Middle: {numbers[1:7]} ")
+print(f"Middle: {numbers[2:6]} ")
 print(f"Reversed: {numbers[::-1]} ")
