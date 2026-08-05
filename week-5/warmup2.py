33d2e000b6f6276b1c9f2a2a51c3b1eccc0004df
kfbmartin <your@email.com>
2026-08-04 22:41:16 -0400

    adding all files for week 5 lesson

---
+++ week-5/warmup2.py
@@ -0,0 +1,18 @@
+while True: 
+    user_input = input("Enter a positive integer: ")
+
+    if user_input.isdigit():
+        if int(user_input) > 0:
+            print(f"Got it: {user_input}")
+            break
+
+        elif int(user_input) <=0:
+            print("That's not a positive integer. Try again.")
+
+    else:
+        print("That's not a positive integer. Try again.")
+
+
+
+
+
