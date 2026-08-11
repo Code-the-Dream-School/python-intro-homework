e461ec5dc5f2e3683f43608147244c109b98e0d8
kfbmartin <your@email.com>
2026-08-11 19:09:35 -0400

    fixing warmup2 pull print statemsnts out of functions

--- week-6/assignment-6/warmup2.py
+++ week-6/assignment-6/warmup2.py
@@ -1,11 +1,13 @@
 def celsius_to_fahrenheit(c):
     fahrenheit = round(((c * 9/5) + 32) , 1)
-    print(f"{c}\xb0C = {fahrenheit}\xb0F")
+    #print(f"{c}\xb0C = {fahrenheit}\xb0F")
+    return fahrenheit
 
 def fahrenheit_to_celsius(f):
     celsius = round(((f - 32) * 5/9), 1)
-    print(f"{f}\xb0F = {celsius}\xb0C")
+    return celsius
+    #print(f"{f}\xb0F = {celsius}\xb0C")
 
-celsius_to_fahrenheit(0)
-celsius_to_fahrenheit(100)
-fahrenheit_to_celsius(72)
+print(f"0\xb0C to {celsius_to_fahrenheit(0)}\xb0F")
+print(f"100\xb0C to {celsius_to_fahrenheit(100)}\xb0F")
+print(f"72\xb0C to {fahrenheit_to_celsius(72)}\xb0F")
